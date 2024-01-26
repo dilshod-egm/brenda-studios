f3452d2bd1b56b0e9611f39cb8da428897cc993b
Dilshod Egm <dilshodegamnazarov21@gmail.com>
2024-01-26 13:50:33 +0500

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,34 @@
+// Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
+// import { isMobile } from "./functions.js";
+// import { formsModules } from "./forms/forms.js";
+
+// Barba.js //
+// barba.init({
+//       // ...
+//     })
+
+// Mouse Move Animation //
+const gallery = document.getElementById("gallery");
+
+window.onmousemove = e => {
+  const mouseX = e.clientX,
+        mouseY = e.clientY;
+  
+  const xDecimal = mouseX / window.innerWidth,
+        yDecimal = mouseY / window.innerHeight;
+  
+  const maxX = gallery.offsetWidth - window.innerWidth,
+        maxY = gallery.offsetHeight - window.innerHeight;
+  
+  const panX = maxX * xDecimal * -1,
+        panY = maxY * yDecimal * -1;
+  
+  gallery.animate({
+    transform: `translate(${panX}px, ${panY}px)`
+  }, {
+    duration: 5000,
+    fill: "forwards",
+    easing: "ease"
+  })
+};
+
